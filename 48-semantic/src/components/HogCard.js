e08cf04a3b4dae9c4d458c399c90937fcc8e3a20
Sam Barker <samuel.e.barker@googlemail.com>
2019-08-12 15:05:38 +0100

    after lecture with some custom semantic stuff

--- 48-semantic/src/components/HogCard.js
+++ 48-semantic/src/components/HogCard.js
@@ -24,7 +24,6 @@ const backExtra = (props, hideDetails, onCheckboxChange) => (<>
         <Icon name='weight' />
         {props["weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"]}
     </Card.Content>
-
     <Card.Content>
         <Checkbox
             label='Greased'
--- 48-semantic/src/index.js
+++ 48-semantic/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-less/semantic.less'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
